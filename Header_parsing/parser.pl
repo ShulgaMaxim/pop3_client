aee27cc03b3aa74607c9022f95df5d159dfb2396
Shulga Max <mslulgaa@gmail.com>
2013-06-25 15:09:42 +0600

    add tests for parser.pl

--- Header_parsing/parser.pl
+++ Header_parsing/parser.pl
@@ -7,30 +7,33 @@ $|++;
 
 my %h_head;
 
-open(F,"file");
-my $text;
-while (<F>) {
-	$text.=$_;
+for (my $i = 1 ; $i <= 4; $i++) {
+	my $file = "file".$i;
+	print $file."\n";
+	open F,"< $file" or die $!;
+	my $text;
+	while (<F>) {
+		$text.=$_;
+	}
+
+	my @res = split ("\n\n", $text);
+	my $head = $res[0];
+
+	@res = split("\n", $head);
+
+	my @length = split (" ",$res[0],3);
+	my $length = $length[1];
+	for my $field (keys @res) {
+		my @a = split(":", $res[$field], 2);
+		$h_head{$a[0]} = $a[1];	
+	}
+	
+	my $Subject = decode("MIME-Header", $h_head{'Subject'});
+
+
+	print "From : $h_head{'From'}\n";
+	print "To : $h_head{'To'}\n";
+	print "Date : $h_head{'Date'}\n";
+	print "Subject : $Subject\n";
+	print "Length : $length\n";
 }
-
-my @res = split ("\n\n", $text);
-my $head = $res[0];
-
-@res = split("\n", $head);
-
-my @length = split (" ",$res[0],3);
-my $length = $length[1];
-for my $field (keys @res) {
-	my @a = split(":", $res[$field], 2);
-	$h_head{$a[0]} = $a[1];	
-}
-
-my $Subject = decode("MIME-Header", $h_head{'Subject'});
-
-
-print "From : $h_head{'From'}\n";
-print "To : $h_head{'To'}\n";
-print "Date : $h_head{'Date'}\n";
-print "Subject : $Subject\n";
-print "Length : $length\n";
-
