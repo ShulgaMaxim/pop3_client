5ae93721a534c667f8ce203df52e5af22c6981a9
Shulga Max <mslulgaa@gmail.com>
2013-06-25 17:04:58 +0600

    add test

--- Header_parsing/parser.pl
+++ Header_parsing/parser.pl
@@ -1,4 +1,4 @@
-
+#!/usr/bin/perl
 use strict;
 use Socket;
 use MIME::Base64;
@@ -7,7 +7,7 @@ $|++;
 
 my %h_head;
 
-for (my $i = 1 ; $i <= 4; $i++) {
+for (my $i = 1 ; $i <= 5; $i++) {
 	my $file = "file".$i;
 	print $file."\n";
 	open F,"< $file" or die $!;
@@ -27,13 +27,14 @@ for (my $i = 1 ; $i <= 4; $i++) {
 		my @a = split(":", $res[$field], 2);
 		$h_head{$a[0]} = $a[1];	
 	}
-	
+	my @from = split (" ", $h_head{'From'}, 2);
+	$from[0] = decode("MIME-Header", $from[0]);
 	my $Subject = decode("MIME-Header", $h_head{'Subject'});
+	my $count = scalar (@from);
 
-
-	print "From : $h_head{'From'}\n";
+	print "From : @from\n";
 	print "To : $h_head{'To'}\n";
 	print "Date : $h_head{'Date'}\n";
 	print "Subject : $Subject\n";
-	print "Length : $length\n";
+	print "\n\n";
 }
